b00b486c13dea3dffa7e8125c697fd673eea36ce
victorwwang(王汪) <victorwwang@tencent.com>
2016-06-14 10:30:56 +0800

    1. 研读index.js文件。

--- index.android.js
+++ index.android.js
@@ -1,6 +1,9 @@
 'use strict';
+// 使用 Javascript 严格模式，区别于正常模式，参考 http://www.ruanyifeng.com/blog/2013/01/javascript_strict_mode.html
 
 var React = require('react-native');
+// 引用 react-native 模块，ES5语法
+// ES5 ES6 语法对比，参考 http://bbs.reactnative.cn/topic/15/react-react-native-%E7%9A%84es5-es6%E5%86%99%E6%B3%95%E5%AF%B9%E7%85%A7%E8%A1%A8
 
 var {
   AppRegistry,
@@ -10,6 +13,7 @@ var {
   WebView,
   BackAndroid
 } = React;
+// 引用 react-native 的具体模块，ES5语法
 
 var _navigator;
 
@@ -17,7 +21,7 @@ var ToolbarAndroid = require('ToolbarAndroid');
 
 var Dashboard = require('./App/Views/Dashboard/index.android.js');
 var Post = require('./App/Views/Post/index.android.js');
-
+// 引用导出模块，ES5语法
 
 BackAndroid.addEventListener('hardwareBackPress', () => {
   if (_navigator.getCurrentRoutes().length === 1  ) {
@@ -27,13 +31,18 @@ BackAndroid.addEventListener('hardwareBackPress', () => {
   return true;
 });
 
+// 定义组件，ES5语法
 var HackerNews = React.createClass({
+  // 给组件定义方法，ES5语法
   render: function() {
     return (
+      // Navigator 组件使用，参考 http://bbs.reactnative.cn/topic/20/%E6%96%B0%E6%89%8B%E7%90%86%E8%A7%A3navigator%E7%9A%84%E6%95%99%E7%A8%8B
       <Navigator
         style={styles.container}
         tintColor='#FF6600'
+        // 设置 默认页面
         initialRoute={{id: 'Dashboard'}}
+        // 设置 路由
         renderScene={this.navigatorRenderScene}/>
     );
   },
@@ -76,3 +85,4 @@ var styles = StyleSheet.create({
 AppRegistry.registerComponent('HackerNews', () => HackerNews);
 
 module.exports = HackerNews;
+// 导出模块，ES5语法
